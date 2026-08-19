9035c5edd03759967f3d61fb06ba951768302ba9
Hitesh Yogesh Kumawat <momsofficial989@gmail.com>
2026-08-18 22:41:14 -0700

    Add new allowed origin for CORS

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "http://192.168.43.185:8080",
+        "https://clean-ai-seven.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
